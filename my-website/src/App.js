ca3286b0e41f516f1882b722d4b84b8e14e24d9c
Mark Paguay <mpaguay927@gmail.com>
2022-07-14 01:26:38 -0400

    Changed Words

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -5,11 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1> Mark&apos;s Website</h1>
+
+	<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
